f7be4e45bc1dbc7bcc706531f6e1dc780a23d927
Jacob Hoke <jacob@threads13.com>
2018-02-05 17:37:31 -0500

    install axios and lodash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostShow from './components/posts_show';
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
